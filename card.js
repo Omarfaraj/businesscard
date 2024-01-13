c8b19e027e7f610d7d938cac39daa984f0423e2b
Faraj <omarfaraj355@gmail.com>
2024-01-13 14:07:24 -0500

    a business card, to start with

---
+++ card.js
@@ -0,0 +1,5 @@
+const name = "Faraj Omar";
+const jobTitle = "Junior Front-end Developer";
+const contactInfo = "omarfaraj3552gmail.com/ +254705943110";
+
+document.getElementById("contactInfo").inner.HTML = contactInfo;
